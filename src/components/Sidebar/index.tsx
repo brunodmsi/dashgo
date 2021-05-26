db98243e97e0cdf49b59183a14f9ae0531aff871
brunodmsi <brunodemasi1@gmail.com>
2021-05-26 13:41:42 -0300

    feat(Sidebar): drawer context to open and close

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,9 +9,13 @@ import {
 	useBreakpointValue
 } from "@chakra-ui/react";
 
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+
 import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
+	const { isOpen, onClose } = useSidebarDrawer();
+
 	const isDrawerSidebar = useBreakpointValue({
 		base: true,
 		lg: false,
@@ -19,7 +23,7 @@ export function Sidebar() {
 
 	if (isDrawerSidebar) {
 		return (
-			<Drawer isOpen={false} placement="left" onClose={() => {}}>
+			<Drawer isOpen={isOpen} placement="left" onClose={onClose}>
 				<DrawerOverlay>
 					<DrawerContent bg="gray.800" p="4">
 						<DrawerCloseButton mt="6" />
