9157488793ca7ece766370fbdce6162c53444968
brunodmsi <brunodemasi1@gmail.com>
2021-06-01 14:10:13 -0300

    feat(UserList): listing users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -28,7 +28,18 @@ export default function UserList() {
 		const response = await fetch('http://localhost:3000/api/users')
 		const data = await response.json();
 
-		return data;
+		const users = data.users.map(user => ({
+			id: user.id,
+			name: user.name,
+			email: user.email,
+			createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			}),
+		}));
+
+		return users;
 	});
 
 	const isWideVersion = useBreakpointValue({
@@ -81,18 +92,22 @@ export default function UserList() {
 									</Tr>
 								</Thead>
 								<Tbody>
-									<Tr>
-										<Td px={['4', '4', '6']} >
-											<Checkbox colorScheme="pink" />
-										</Td>
-										<Td px="6">
-											<Box>
-												<Text fontWeight="bold">Bruno De Masi</Text>
-												<Text fontSize="sm" color="gray.300">brunodemasi1@gmail.com</Text>
-											</Box>
-										</Td>
-										{isWideVersion && <Td px="6">04 de Abril, 2021</Td>}
-									</Tr>
+									{data.map(user => {
+										return (
+											<Tr key={user.id}>
+												<Td px={['4', '4', '6']} >
+													<Checkbox colorScheme="pink" />
+												</Td>
+												<Td px="6">
+													<Box>
+														<Text fontWeight="bold">{user.name}</Text>
+														<Text fontSize="sm" color="gray.300">{user.email}</Text>
+													</Box>
+												</Td>
+												{isWideVersion && <Td px="6">{user.createdAt}</Td>}
+											</Tr>
+										)
+									})}
 								</Tbody>
 							</Table>
 
