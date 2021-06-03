adabe958d971f9d3802d48647bbeb5a641b44748
brunodmsi <brunodemasi1@gmail.com>
2021-06-03 12:25:32 -0300

    feat: using axios for fetching api

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,6 +18,7 @@ import {
 import { RiAddLine } from "react-icons/ri";
 import Link from 'next/link';
 import { useQuery } from 'react-query';
+import { api } from '../../services/api';
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -25,8 +26,7 @@ import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
 	const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-		const response = await fetch('http://localhost:3000/api/users')
-		const data = await response.json();
+		const { data } = await api.get('/users')
 
 		const users = data.users.map(user => ({
 			id: user.id,
