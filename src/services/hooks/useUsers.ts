dd4ab5a73b17f585d5b0e46cab73443397e9ba59
brunodmsi <brunodemasi1@gmail.com>
2021-06-03 14:40:04 -0300

    fix: removing non existing options

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -42,6 +42,5 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
 export function useUsers(page: number) {
 	return useQuery(['users', page], () => getUsers(page), {
 		staleTime: 1000 * 60 * 10, // 10 minutes
-		...options,
 	});
 }
