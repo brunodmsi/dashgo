551962ac7ed54e8f74c43f6b689c0cd4f7d0e529
brunodmsi <brunodemasi1@gmail.com>
2021-06-03 13:28:41 -0300

    feat(Pagination): dynamic page showing and treating data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -98,7 +98,11 @@ export default function UserList() {
 								</Tbody>
 							</Table>
 
-							<Pagination />
+							<Pagination
+								totalCountOfRegisters={200}
+								currentPage={4}
+								onPageChange={() => {}}
+							/>
 						</>
 					)}
         </Box>
